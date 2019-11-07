bc625c293fb23f92f2ac689e2dc55fd1343af684
Intese Avarice <gian_mauricio@dlsu.edu.ph>
2019-11-07 19:44:56 +0800

    FUK

--- MatrixTester.java
+++ MatrixTester.java
@@ -89,30 +89,116 @@ public class MatrixTester
                     break;
 
                 case 2:/*Scaling*/
+                    System.out.println("Enter how much to translate in x: ");
+                    double ScaleX = scan.nextDouble();
+                    System.out.println("Enter how much to translate in y: ");
+                    double ScaleY = scan.nextDouble();
+                    System.out.println("Enter how much to translate in z: ");
+                    double ScaleZ = scan.nextDouble();
+
                     System.out.println("[1] Object space");
                     System.out.println("[2] World space");
-
                     operation = scan.nextInt();
 
                     if(operation == 1){
+                        matrix Center = composeTransform.baryCenter(composeTransform);
+                        matrix Return = composeTransform.baryReturn(composeTransform);
+                        matrix Scale = composeTransform.Createscale(ScaleX, ScaleY, ScaleZ);
 
+                        composeTransform = Center.mult(composeTransform);
+                        composeTransform = Scale.mult(composeTransform);
+                        composeTransform = Return.mult(composeTransform);
                     }
 
                     else{
-
+                        matrix Scale = composeTransform.Createscale(ScaleX, ScaleY, ScaleZ);
+                        composeTransform = Scale.mult(composeTransform);
                     }
                     break;
 
                 case 3:/*Distort*/
+                    System.out.println("[1] Distort in x");
+                    System.out.println("[2] Distort in y");
+                    System.out.println("[3] Distort in z");
+                    int nAxis = scan.nextInt();
+
+                    System.out.println("Distort by how much?");
+                    double Distort = scan.nextDouble();
+
+                    System.out.println("[1] Object space");
+                    System.out.println("[2] World space");
+                    operation = scan.nextInt();
+
+                    if(operation == 1){
+                        matrix Center = composeTransform.baryCenter(composeTransform);
+                        matrix Return = composeTransform.baryReturn(composeTransform);
+                        matrix Dist = composeTransform.Createskew(nAxis, Distort);
+
+                        composeTransform = Center.mult(composeTransform);
+                        composeTransform = Dist.mult(composeTransform);
+                        composeTransform = Return.mult(composeTransform);
+                    }
+
+                    else{
+                        matrix Dist = composeTransform.Createskew(nAxis, Distort);
+                        composeTransform = Dist.mult(composeTransform);
+                    }
                     break;
 
                 case 4:/*Squeeze*/
+                    System.out.println("Squeeze by how much?");
+                    double Squish = scan.nextDouble();
+
+                    System.out.println("[1] Object space");
+                    System.out.println("[2] World space");
+                    operation = scan.nextInt();
+
+                    if(operation == 1){
+                        matrix Center = composeTransform.baryCenter(composeTransform);
+                        matrix Return = composeTransform.baryReturn(composeTransform);
+                        matrix Squeeze = composeTransform.createSqueeze(Squish);
+
+                        composeTransform = Center.mult(composeTransform);
+                        composeTransform = Squeeze.mult(composeTransform);
+                        composeTransform = Return.mult(composeTransform);
+                    }
+
+                    else{
+                        matrix Squeeze = composeTransform.createSqueeze(Squish);
+                        composeTransform = Squeeze.mult(composeTransform);
+                    }
                     break;
 
                 case 5:/*Projection*/
                     break;
 
                 case 6:/*Rotation*/
+                    System.out.println("[1] Rotate in x");
+                    System.out.println("[2] Rotate in y");
+                    System.out.println("[3] Rotate in z");
+                    int nRot = scan.nextInt();
+
+                    System.out.println("Rotate by how much? (in Radians)");
+                    double Angle = scan.nextDouble();
+
+                    System.out.println("[1] Object space");
+                    System.out.println("[2] World space");
+                    operation = scan.nextInt();
+
+                    if(operation == 1){
+                        matrix Center = composeTransform.baryCenter(composeTransform);
+                        matrix Return = composeTransform.baryReturn(composeTransform);
+                        matrix Spin = composeTransform.CreateSpin(nRot, Angle);
+
+                        composeTransform = Center.mult(composeTransform);
+                        composeTransform = Spin.mult(composeTransform);
+                        composeTransform = Return.mult(composeTransform);
+                    }
+
+                    else{
+                        matrix Spin = composeTransform.CreateSpin(nRot, Angle);
+                        composeTransform = Spin.mult(composeTransform);
+                    }
                     break;
             }
         }while(operation != 7);
